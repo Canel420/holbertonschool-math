fb1ae5898588d602d33900758c2388f7e97dedae
Canel420 <4521@holbertonschool.com>
2022-03-28 13:03:48 -0500

    Output correction

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include "holberton.h"
 
+/**
+ * display_complex_number - Function that displays complex numbers
+ *
+ * @c: name of complex number.
+ *
+ * Description: Display complex number according to complex sctructure
+ * where c.re defines the real part number and c.im defines the imaginary
+ * part.
+ *
+ */
 void display_complex_number(complex c)
 {
 	int integerPart = (int)c.re;
@@ -8,6 +18,8 @@ void display_complex_number(complex c)
 
 	if (c.im == 0)
 		printf("%d\n", integerPart);
+	if (c.im < 0)
+		printf("%d - %di\n", integerPart, integerParti);
 	else
 		printf("%d + %di\n", integerPart, integerParti);
 }
--- 0x00-math_complex/holberton.h
+++ 0x00-math_complex/holberton.h
@@ -1,6 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/**
+ * struct complex - Structure for complex numbers
+ *
+ * @re: defines a double type to the real part.
+ * @im: defines a double type to the imaginary part
+ *
+ */
+
 typedef struct complex
 {
 	double re;
