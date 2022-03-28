610998424d30954502d61ea2d51d7af29d1eab85
Canel420 <4521@holbertonschool.com>
2022-03-28 13:23:06 -0500

    Fixed negative output

--- 0x00-math_complex/0-main.c
+++ 0x00-math_complex/0-main.c
@@ -17,5 +17,14 @@ int main(void)
 	c1.re = 1;
 	c1.im = 0;
 	display_complex_number(c1);
+	c1.re = 2;
+	c1.im = -1;
+	display_complex_number(c1);
+	c1.re = 1;
+	c1.im = 1;
+	display_complex_number(c1);
+	c1.re = 1;
+	c1.im = -2;
+	display_complex_number(c1);
 	return (0);
 }
